18624c06dd33660c7436d984d4900f06670cbf6f
Cat Gomez <catherinedgomez@gmail.com>
2020-07-17 14:58:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,6 @@ require 'pry'
 def reverse_each_word(string)
   reversed_sentence = []
   reversed_sentence << string.split(" ").map(&:reverse).join(" ")
+  reversed_sentence.join
   # binding.pry
 end
